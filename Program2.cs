ad58a824b3c3f0f07ab4c171c6239bae3a3cd239
ramer klipel <ramerh@outlook.com>
2025-02-22 16:17:47 -0300

    done

--- Program2.cs
+++ Program2.cs
@@ -1,14 +1,21 @@
 ﻿// O desafio é criar um programa que me diga se a entrada é um palíndromo ou não
-
+namespace Palíndromo;
 class Program
 {
-    public static void main ()
+    public static void Main ()
     {
         Console.Write("é um palíndromo ou não é? eis a questão! Digite uma palavra e te direi: ");
         string awnser = Console.ReadLine();
         String reverseAwnser = new 
         string(awnser.Reverse().ToArray());  //reverter a palavra e transofrmar em array para reverter
-
+        if (reverseAwnser.Equals(awnser, StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine("É um palíndromo");
+        }
+        else
+        {
+            Console.WriteLine("não é um palíndromo");
+        }
 
     }
 }
